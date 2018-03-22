9506121c0be75fa8637ef18093e526e53999d5f8
dujuan.du <dujuan.du@qunar.com>
2018-03-22 19:50:57 +0800

    feat: +

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -53,7 +53,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      // filename: 'index.html',
       template: 'index.html',
       inject: true
     }),

2ea5f95f18441394043788c107654a2eea4bcb0b
dujuan.du <dujuan.du@qunar.com>
2018-03-26 16:15:06 +0800

    feat: cli upgrade & element ui

---
+++ src/components/addressSelect.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="address-wrapper">
+    <el-select v-model="province"></el-select>
+    <el-select v-model="city"></el-select>
+    <el-select v-model="district"></el-select>
+  </div>
+</template>
+
+<script>
+
+import DISTRICTS from '../assets/districts.js';
+export default {
+  name: 'addressSelect',
+  data () {
+    return {
+      province: '',
+      city: '',
+      district: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
